8e5214aa20e09d77838df23d16b6bc64210dace2
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-20 15:27:15 +0530

    Completed frontend Login functionality.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Login = () => {
+const Login = ({setUserAuth}) => {
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(userName && password){
+            setUserAuth(true);
+        }else{
+            setError('Please enter both username and password.')
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1 className="mb-4">Login</h1>
+                {error && (
+                    <pre className="border rounded alert alert-danger">
+                        {error}
+                    </pre>
+                )}
                 <div className="mb-3" style={{width: '400px'}}>
                     <label HTMLfor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label HTMLfor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input type="password" className="form-control" id="exampleInputPassword1"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form> 
