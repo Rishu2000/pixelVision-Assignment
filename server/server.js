4bf3d11cfa22b7287e45c13cb4a7210ffc172b3b
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-20 17:20:05 +0530

    Removed unnessasary code.

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json('server is running')          //data[1].username
+    res.json('server is running')          
 })
 
 app.post('/', (req, res) => {
