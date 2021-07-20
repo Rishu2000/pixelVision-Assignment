3e63d67e304411068457acab7693debfbffdfaa5
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-20 11:41:06 +0530

    Created Login Component and Implemented Switching functionality from react-router-dom.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
 import "./App.css"
+import Login from './Login'
 
 const App = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center'}} className="pt-5">
-      <button className="btn btn-primary btn-lg">
-        Login
-      </button>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Link to="/login">
+            <button className="btn btn-primary btn-lg">
+              Login
+            </button>
+          </Link>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+      </Switch>
     </div>
   )
 }
