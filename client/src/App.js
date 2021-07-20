717fdba94e1b70c29f50f41b1383229162e817fd
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-20 15:26:20 +0530

    Removed unnesssasary Router part.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom'
+import React, {useState} from 'react'
 import "./App.css"
 import Login from './Login'
+import Welcome from './Welcome'
 
 const App = () => {
+
+  const [userAuth, setUserAuth] = useState(false);
+
   return (
     <div style={{display: 'flex', justifyContent: 'center'}} className="pt-5">
-      <Switch>
-        <Route path="/" exact={true}>
-          <Link to="/login">
-            <button className="btn btn-primary btn-lg">
-              Login
-            </button>
-          </Link>
-        </Route>
-        <Route path="/login">
-          <Login/>
-        </Route>
-      </Switch>
+      {!userAuth?(
+        <Login setUserAuth={setUserAuth}/>
+      ):(
+        <Welcome/>
+      )}
     </div>
   )
 }
